b0f5814965ed7eea60552ef556da078902ec5ccd
Andres Arnedo <andresarnedo2002@gmail.com>
2023-06-27 00:21:13 -0500

    T&C

---
+++ pages/legal/index.jsx
@@ -0,0 +1,147 @@
+import React from "react";
+
+export default function TermsAndConditions() {
+  return (
+    <div className="max-w-3xl mx-auto px-4 py-8">
+      <h1 className="text-2xl font-bold mb-4">
+        Términos y Condiciones de la Aplicación para Intermediación de
+        Trabajadores Informales
+      </h1>
+      <p className="mb-4">Fecha de entrada en vigencia: 20 de junio de 2023</p>
+
+      <p className="mb-4">
+        Estos Términos y Condiciones ("T&C") establecen los derechos y
+        obligaciones legales entre los usuarios ("Usuario", "Usted", "Cliente" o
+        "Trabajador") y la aplicación de intermediación de trabajadores
+        informales ("Aplicación", "Nosotros", "Nos" o "Plataforma") llamada
+        Cameya S.A.S, que facilita la contratación de trabajadores para
+        servicios informales. Al acceder y utilizar nuestra Aplicación, usted
+        acepta cumplir con estos T&C en su totalidad. Si no está de acuerdo con
+        alguno de estos términos, le recomendamos no utilizar nuestra
+        Aplicación.
+      </p>
+
+      <h2 className="text-xl font-bold mb-2">1. Uso de la Aplicación</h2>
+
+      <p className="mb-4">
+        1.1 Elegibilidad: Usted declara y garantiza que tiene al menos 18 años
+        de edad y cuenta con la capacidad legal para celebrar acuerdos
+        vinculantes.
+      </p>
+
+      <p className="mb-4">
+        1.2 Propósito: Nuestra Aplicación tiene como objetivo ser un
+        intermediario en la contratación de trabajadores informales. Los
+        Clientes pueden publicar trabajos y los Trabajadores pueden ofrecerse
+        para realizarlos. La Aplicación no es responsable de la ejecución o
+        calidad de los servicios proporcionados por los Trabajadores. La
+        relación contractual se establece únicamente entre el Cliente y el
+        Trabajador.
+      </p>
+
+      <p className="mb-4">
+        1.3 Responsabilidad del Usuario: Usted es responsable de la exactitud de
+        la información proporcionada en la Aplicación. No podemos verificar ni
+        garantizar la veracidad de los datos proporcionados por los Usuarios.
+      </p>
+
+      <h2 className="text-xl font-bold mb-2">
+        2. Publicación y Contratación de Trabajos
+      </h2>
+
+      <p className="mb-4">
+        2.1 Publicación de Trabajos: Los Clientes pueden publicar trabajos
+        especificando los detalles del servicio requerido, incluyendo la
+        descripción del trabajo, ubicación, fecha, duración y tarifa ofrecida.
+      </p>
+
+      <p className="mb-4">
+        2.2 Ofertas de Trabajadores: Los Trabajadores pueden ofrecerse para
+        realizar los trabajos publicados. Los Trabajadores deben proporcionar
+        información precisa sobre su experiencia, habilidades y tarifa
+        solicitada.
+      </p>
+
+      <p className="mb-4">
+        2.3 Elección del Trabajador: El Cliente tiene la responsabilidad de
+        seleccionar al Trabajador más adecuado para el trabajo, basándose en la
+        información proporcionada por los Trabajadores y cualquier otro criterio
+        relevante. La Aplicación no garantiza la disponibilidad o calidad de los
+        Trabajadores.
+      </p>
+
+      <h2 className="text-xl font-bold mb-2">3. Tarifas y Pagos</h2>
+
+      <p className="mb-4">
+        3.1 Tarifas: La Aplicación puede cobrar tarifas por el uso de la
+        plataforma. Estas tarifas se establecerán claramente antes de la
+        confirmación de un trabajo. Los detalles de las tarifas y los pagos se
+        proporcionarán en el apartado correspondiente de la Aplicación.
+      </p>
+
+      <p className="mb-4">
+        3.2 Pagos al Trabajador: El Cliente se compromete a pagar al Trabajador
+        la tarifa acordada por los servicios prestados. El pago se realizará de
+        acuerdo con los términos acordados entre el Cliente y el Trabajador. La
+        Aplicación no es responsable de la realización o procesamiento de los
+        pagos.
+      </p>
+
+      <h2 className="text-xl font-bold mb-2">
+        4. Responsabilidades y Limitaciones
+      </h2>
+
+      <p className="mb-4">
+        4.1 Responsabilidades del Usuario: Usted acepta utilizar la Aplicación
+        de manera legal, respetando los derechos de los demás Usuarios. Usted
+        será el único responsable de cualquier contenido que publique o envíe a
+        través de la Aplicación.
+      </p>
+
+      <p className="mb-4">
+        4.2 Limitación de Responsabilidad: La Aplicación no será responsable de
+        ningún daño directo, indirecto, incidental, especial o consecuente
+        derivado del uso de la Aplicación, incluyendo pérdida de beneficios,
+        datos, reputación o daños a la propiedad.
+      </p>
+
+      <h2 className="text-xl font-bold mb-2">
+        5. Modificaciones y Terminación
+      </h2>
+
+      <p className="mb-4">
+        5.1 Modificaciones: Nos reservamos el derecho de modificar estos T&C en
+        cualquier momento. Le notificaremos cualquier cambio a través de la
+        Aplicación o por otros medios razonables. El uso continuado de la
+        Aplicación después de la modificación de los T&C se considerará como
+        aceptación de los mismos.
+      </p>
+
+      <p className="mb-4">
+        5.2 Terminación: Nos reservamos el derecho de terminar o suspender su
+        acceso a la Aplicación en caso de incumplimiento de estos T&C o por
+        cualquier otra razón justificada.
+      </p>
+
+      <h2 className="text-xl font-bold mb-2">6. Disposiciones Generales</h2>
+
+      <p className="mb-4">
+        6.1 Ley Aplicable: Estos T&C se regirán e interpretarán de acuerdo con
+        las leyes de Colombia.
+      </p>
+
+      <p className="mb-4">
+        6.2 Resolución de Conflictos: Cualquier disputa o reclamo relacionado
+        con estos T&C se resolverá mediante negociaciones de buena fe. Si no es
+        posible llegar a un acuerdo, las partes aceptan someterse a la
+        jurisdicción exclusiva de los tribunales competentes en Cartagena,
+        Colombia.
+      </p>
+
+      <p className="mb-4">
+        Declaro que he leído y entendido los presentes Términos y Condiciones, y
+        acepto cumplir con ellos al utilizar la Aplicación de Cameya S.A.S.
+      </p>
+    </div>
+  );
+}
