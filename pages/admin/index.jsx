491aa38c0869eae84bddd25b3abfebed7179b30e
Kevin Martinez <kmartinez@liveventures.com>
2023-06-26 22:54:26 -0500

    Feat: changes on admin login

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,8 +1,20 @@
 import AdminLoginForm from "components/AdminLoginForm";
 import { IoArrowBackOutline } from 'react-icons/io5'
 import Link from "next/link";
+import { useSelector } from 'react-redux';
+import React, {useEffect} from "react";
+import { useRouter } from "next/router";
+import PageRoutes from "constants/routes/pages";
 
 export default function AdminLogin(){
+
+	const user = useSelector(state => state.admin.value.data)
+	const { push } = useRouter()
+
+	useEffect(() => {
+		if(user)
+			push(PageRoutes.admin.dashboard)
+	}, [])
 
 	return(
 		<div className="min-h-screen flex relative justify-center items-center bg-gradient-to-b from-blue-200 to-blue-300">
